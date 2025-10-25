9cd377bb0335f0b76adce1fdc64da02f446a9d82
Salkin <99216108+SteakSalkin@users.noreply.github.com>
2025-10-25 22:03:40 +0200

    added controller control

--- player/player2.gd
+++ player/player2.gd
@@ -2,11 +2,12 @@ class_name Player2
 
 extends CharacterBody2D
 
-const SPEED = 1200.0
-const JUMP_VELOCITY = -600.0
-const MAX_JUMP_VELOCITY = -800.0
+const SPEED = 800.0
+const JUMP_VELOCITY = -400.0
+const MAX_JUMP_VELOCITY = -600.0
 const MAX_HEALTH = 20.0
 const HEAL_AMOUNT = 2.0
+const DAMAGE_AMOUNT = 2.0
 
 var is_flipped = false
 var health = MAX_HEALTH
@@ -15,21 +16,36 @@ var is_touching_healing = false
 var backflip_rotation = 0.0
 var attack_rotation = 0.0
 var attack_animation_wait = 0.0
+var attack_cooldown = 0
+
+var button_left = false
+var button_up = false
+var button_right = false
+var button_down = false
+var button_hit = false
+
+var direction
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor() and not Input.is_action_pressed("player2up"):
+	if not is_on_floor() and not button_up:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_pressed("player2up") and velocity.y > MAX_JUMP_VELOCITY:
+	if button_up and velocity.y > MAX_JUMP_VELOCITY:
 		velocity.y += JUMP_VELOCITY
-	if Input.is_action_pressed("player2down") and velocity.y > MAX_JUMP_VELOCITY:
+	if button_down and velocity.y > MAX_JUMP_VELOCITY:
 		velocity.y -= JUMP_VELOCITY/2
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("player2left", "player2right")
+	#var direction := Input.get_axis("player1left", "player1right")
+	if (button_left):
+		direction = -1.0
+	elif (button_right):
+		direction = 1.0
+	else:
+		direction = 0
 	if direction:
 		velocity.x = direction * SPEED
 		if direction > 0 and is_flipped:
@@ -41,10 +57,11 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	if Input.is_action_just_pressed("player2attack") and is_touching_player:
-		animation_taking_damage()
-		get_node("/root/level/Player").animation_attack()
-		get_damage(1)
+	if button_hit and is_touching_player:
+		button_hit = false
+		get_node("/root/level/Player").animation_taking_damage()
+		animation_attack()
+		get_node("/root/level/Player").get_damage(DAMAGE_AMOUNT)
 
 	if is_touching_healing:
 		get_healed(HEAL_AMOUNT * delta)
@@ -97,3 +114,33 @@ func _on_healbox_area_entered(area: Area2D) -> void:
 
 func _on_healbox_area_exited(area: Area2D) -> void:
 	is_touching_healing = false
+
+func _on_button_left_area_entered(area: Area2D) -> void:
+	button_left = true
+
+func _on_button_left_area_exited(area: Area2D) -> void:
+	button_left = false
+
+func _on_button_up_area_entered(area: Area2D) -> void:
+	button_up = true
+
+func _on_button_up_area_exited(area: Area2D) -> void:
+	button_up = false
+
+func _on_button_right_area_entered(area: Area2D) -> void:
+	button_right = true
+
+func _on_button_right_area_exited(area: Area2D) -> void:
+	button_right = false
+
+func _on_button_down_area_entered(area: Area2D) -> void:
+	button_down = true
+
+func _on_button_down_area_exited(area: Area2D) -> void:
+	button_down = false
+
+func _on_button_hit_area_entered(area: Area2D) -> void:
+	button_hit = true
+
+func _on_button_hit_area_exited(area: Area2D) -> void:
+	button_hit = false
