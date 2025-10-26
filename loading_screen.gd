d629bcc2405edddba8d2a9f20a588d4fd4ece7a1
janine544 <doering.janine@gmx.net>
2025-10-26 13:16:55 +0100

    combat sound

--- loading_screen.gd
+++ loading_screen.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Zahnrad.rotate(2 * delta)
+	$ Path2D/PathFollow2D/Zahnrad.rotate( 2 * delta)
 
 
 func _on_timer_timeout() -> void:
